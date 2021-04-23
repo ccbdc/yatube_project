f286aeed740e5a12a1baf320a6870bce41da2342
Irina Metelkina <ccbdc@yandex.ru>
2021-04-23 20:41:47 +0300

    added templates, bootstrap, update HTML files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 from .models import Post
 
 
 def index(request):
-# одна строка вместо тысячи слов на SQL
-    latest = Post.objects.order_by('-pub_date')[:10]
-    # собираем тексты постов в один, разделяя новой строкой
-    output = []
-    for item in latest:
-        output.append(item.text)
-    return HttpResponse('\n'.join(output))
+    latest = Post.objects.order_by('-pub_date')[:11]
+    return render(request, 'index.html', {'posts': latest})
+    
